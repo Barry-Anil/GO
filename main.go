1372edb1d0eddc44675a541fba0c82f074c7f709
Barry Anil <annenex.11@gmail.com>
2024-08-06 23:10:23 +0530

    golang lesson 11

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Ninjas!!")
+	var name string = "Barry"
+	var nameTwo = "lee"
+	var nameThree string
+
+	fmt.Println(name, nameTwo, nameThree)
+
+	name = "anil"
+	nameThree = "Barry Anil"
+
+	fmt.Println(name, nameTwo, nameThree)
+
+	nameFour := "7uckingMad!"
+
+	fmt.Println(name, nameTwo, nameThree, nameFour)
+
+	//Integers
+
+	var ageOne int = 20
+	var ageTwo = 30
+	var ageThree int
+	ageFour := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree, ageFour)
+
+
+}

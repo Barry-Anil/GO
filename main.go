2d360f7ede32255daaeccab2b0e5dd90fc3df680
Barry Anil <annenex.11@gmail.com>
2024-08-08 23:22:54 +0530

    Lesson 6: booleans and Conditional Statements

--- main.go
+++ main.go
@@ -3,28 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
-	for x < 5 {
-		fmt.Println("the value of x :", x)
-		x++
+	
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
 	}
 
-	for i := 0; i < 5; i++ {
-		fmt.Println("the value of i :", i)
-	}
-
-	names := []string{"barry", "anil", "lee", "mario"}
-
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
-	}
+	names := []string{"barry", "anil", "lee", "nihayo"}
 
 	for index, value := range names {
-		fmt.Printf("the value at index %v is %v \n", index, value)
+		if index == 1 {
+			fmt.Println("continuing at Pos: ", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("break at pos", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
 	}
 
-	for _, value := range names {
-		fmt.Printf("the value is %v \n", value)
-
-	}
 }

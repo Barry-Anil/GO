ff3359bb8e0bb94981678c18c86dd16a1715d0db
Barry Anil <annenex.11@gmail.com>
2024-08-08 23:53:44 +0530

    Lesson 7: functions

--- main.go
+++ main.go
@@ -1,37 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func  greeting(n string){
+	fmt.Println("Ni hao",n)
+}
+
+func cycleNames(n []string, f func(string)){
+	for _, v := range n {
+		f(v)
+	}
+}
+
+func CircleArea(r float64) float64 {
+	return math.Pi * r * r
+}
 
 func main() {
-	
-	age := 45
+	text := "Hello"
+	greeting(text)
 
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
+	names := []string{"barry", "anil", "lee", "hayashi"}
 
-	if age < 30 {
-		fmt.Println("age is less than 30")
-	} else if age < 40 {
-		fmt.Println("age is less than 40")
-	} else {
-		fmt.Println("age is not less than 45")
-	}
+	cycleNames(names, greeting)
 
-	names := []string{"barry", "anil", "lee", "nihayo"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continuing at Pos: ", index)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("break at pos", index)
-			break
-		}
-
-		fmt.Printf("the value at pos %v is %v \n", index, value)
-	}
+	a1 := CircleArea(3)
+	fmt.Printf("Area of the circle %0.3f",a1)
 
 }

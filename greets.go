d9d6743998f4f62f0e3765417130db0d93dde32b
Barry Anil <annenex.11@gmail.com>
2024-08-09 12:57:26 +0530

    Lesson 9: Package scope

---
+++ greets.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var points = []int{23, 34, 54, 77}
+
+func sayHello(m string){
+	fmt.Println("Nihao", m)
+}
+
+func yourScore(){
+	fmt.Println("your current score is :", score)
+}
--- main.go
+++ main.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials(s string) (string, string) {
-	n := strings.ToUpper(s)
-	names := strings.Split(n, " ")
-
-	var initials []string
-	for _,v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+var score = 98.5
 
 func main() {
-	p, i := getInitials("Barry Anil")
-	fmt.Printf("first name %v and Last name %v", p, i)
+
+	//you cant access var score = 98.5 because its outside the scope and inside main function. 
+
+	sayHello("barry")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	yourScore()
 }

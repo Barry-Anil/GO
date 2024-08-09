c249073384853a027700284104bc800325e4f5c6
Barry Anil <annenex.11@gmail.com>
2024-08-09 13:10:34 +0530

    Lesson 10: Maps

--- main.go
+++ main.go
@@ -4,17 +4,36 @@ import (
 	"fmt"
 )
 
-var score = 98.5
+
 
 func main() {
 
-	//you cant access var score = 98.5 because its outside the scope and inside main function. 
-
-	sayHello("barry")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup" : 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"chocolate Pudding": 3.55,
 	}
 
-	yourScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	for k,v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	records := map[int]string {
+		1 : "barry",
+		2 : "anil",
+		3 : "lee",
+	}
+
+	fmt.Println(records)
+
+	records[1] = "Barry Anil"
+	fmt.Println(records)
+
+	records[2] = "Anil Kapoor"
+	fmt.Println(records)
+
 }

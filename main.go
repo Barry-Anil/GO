36ab046c5279d26d853274a384688dcaa48d229d
Barry Anil <annenex.11@gmail.com>
2024-08-09 17:11:59 +0530

    Lesson 13: Structs

--- main.go
+++ main.go
@@ -4,28 +4,9 @@ import (
 	"fmt"
 )
 
-// GROUP A TYPES ==> strings, ints, floats, arrays, bools, and structs.
-// GROUP B TYPES ==> slices, func, maps
-
-func updateName (n *string) {
-	
-	// De-referencing the pointer to get the value
-	*n = "Lee"
-
-}
-
-
 
 func main() {
-	name := "Anil"
-
-	//Pointers
-	m := &name
-
-	fmt.Println(name)
-	updateName(m)
-	fmt.Println(name)
-
-
+	bill := newBill("arry's bill")
+	fmt.Println(bill)
 }
 

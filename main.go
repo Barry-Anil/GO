d4610de3c36d30bb2dbba00ebb1e6125f420fc0e
Barry Anil <annenex.11@gmail.com>
2024-08-09 15:47:53 +0530

    Lesson 12: Pointers

--- main.go
+++ main.go
@@ -7,42 +7,24 @@ import (
 // GROUP A TYPES ==> strings, ints, floats, arrays, bools, and structs.
 // GROUP B TYPES ==> slices, func, maps
 
-// func updateName (n string) {
-// 	n = "Lee"
-// }
+func updateName (n *string) {
+	
+	// De-referencing the pointer to get the value
+	*n = "Lee"
 
-// func main() {
-// 	name := "Anil"
-
-// 	updateName(name)
-// 	// this will print anil as functions stores only the copy of the variable name.
-// 	fmt.Println(name)
-
-// }
-
-func updateName (n string) string {
-	n = "Lee"
-	return n
 }
 
-func updateMenu (x map[string]float64) {
-	x["coffee"] = 12.99
-}
+
 
 func main() {
 	name := "Anil"
 
-	name = updateName(name)
-	// this will print the update value for the var name as we are returning the value from the function.
+	//Pointers
+	m := &name
+
 	fmt.Println(name)
-
-	menu := map[string]float64{
-		"pie" : 6.99,
-		"choco" : 4.99,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
+	updateName(m)
+	fmt.Println(name)
 
 
 }
